70294882dd1e574df83797302eea2915c9e9571e
Maks2712 <maks2712@yahoo.com>
2022-08-10 13:19:45 +0700

    completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+let res='';
+let i=0;
 
+if(num>0){
+while (i<num){
+    res+=string;
+    i++
+}
+}
+else if (num<0){
+   res= 'ERROR'
+}
+
+return res;
 };
 
 // Do not edit below this line
