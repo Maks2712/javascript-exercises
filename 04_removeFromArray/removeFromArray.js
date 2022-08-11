103ec18c8b8d5c12b370acc751d0387a59c7e65e
Maks2712 <maks2712@yahoo.com>
2022-08-11 23:10:52 +0700

    test commit

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...data) {
+for(let i=0;i<array.length;i++){
+    if(data===array[i]){
+        array.splice(i,1);
+    }
+}
 };
 
 // Do not edit below this line
